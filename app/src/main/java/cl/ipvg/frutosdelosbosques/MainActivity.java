6adf040ebb9f94debe980aa164447b408fa1ff30
Trabajo CV <cristian.vasquez2@virginiogomez.cl>
2024-10-07 19:35:21 -0300

    nuevo Proyecto

--- app/src/main/java/cl/ipvg/frutosdelosbosques/MainActivity.java
+++ app/src/main/java/cl/ipvg/frutosdelosbosques/MainActivity.java
@@ -2,13 +2,42 @@ package cl.ipvg.frutosdelosbosques;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.view.View;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private boton button;
+    private Local textView2;
+    private SensorManager smg;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Local = (textView2) findViewById(R.id.Local);
+        boton = (button) findViewById(R.id.boton);
+        smg = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+        boton.setOnclickLIstener(new View.OnClickListener()){
+            @Override
+                    public void onClick(view v){
+                List<Sensor> bosque = smg.getSensorList(Sensor.TYPE_ALL);
+                StringBuilder sb = new StringBuilder();
+                for (Sensor s: bosque){
+                    sb.append(s.getName());
+                }
+                boton.setText(sb);
+            }
+        }
     }
+
+
 }
